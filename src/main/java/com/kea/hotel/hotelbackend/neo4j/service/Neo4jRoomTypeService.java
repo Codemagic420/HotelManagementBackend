9882c85f9c2a29289a555d7ea1feef4e2748b147
JoelMultiplaa <joda0002@stud.ek.dk>
2026-06-06 21:27:29 +0200

    Add Neo4j controllers for RoomType and SeasonRate
    - Create Neo4jRoomTypeService and Neo4jRoomTypeController
    - Create Neo4jSeasonRateService and Neo4jSeasonRateController
    - All endpoints follow /api/neo4j/* pattern
    - Implement full CRUD operations
    
    This brings Neo4j coverage to 8/14 entities (57%)

---
+++ src/main/java/com/kea/hotel/hotelbackend/neo4j/service/Neo4jRoomTypeService.java
@@ -0,0 +1,32 @@
+package com.kea.hotel.hotelbackend.neo4j.service;
+
+import com.kea.hotel.hotelbackend.neo4j.node.Neo4jRoomType;
+import com.kea.hotel.hotelbackend.neo4j.repository.Neo4jRoomTypeRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class Neo4jRoomTypeService {
+    private final Neo4jRoomTypeRepository repository;
+
+    public Neo4jRoomTypeService(Neo4jRoomTypeRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Neo4jRoomType> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Neo4jRoomType> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Neo4jRoomType save(Neo4jRoomType roomType) {
+        return repository.save(roomType);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/kea/hotel/hotelbackend/neo4j/service/Neo4jSeasonRateService.java
@@ -0,0 +1,32 @@
+package com.kea.hotel.hotelbackend.neo4j.service;
+
+import com.kea.hotel.hotelbackend.neo4j.node.Neo4jSeasonRate;
+import com.kea.hotel.hotelbackend.neo4j.repository.Neo4jSeasonRateRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class Neo4jSeasonRateService {
+    private final Neo4jSeasonRateRepository repository;
+
+    public Neo4jSeasonRateService(Neo4jSeasonRateRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Neo4jSeasonRate> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Neo4jSeasonRate> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Neo4jSeasonRate save(Neo4jSeasonRate seasonRate) {
+        return repository.save(seasonRate);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
